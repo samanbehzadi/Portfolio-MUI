92702dbbf12daa11aed17b026831280ac477d657
Saman Behzadi <nima23behzadi@outlook.com>
2024-04-19 10:03:57 +0430

    Deploay the project on GitHub Server

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename='contacts-manager-app'>
             <App />
         </BrowserRouter>
     </React.StrictMode>
